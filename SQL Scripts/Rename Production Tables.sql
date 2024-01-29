fc887ee8614e2089238afe1fb2d46c1f75ffb715
KSwaviman <kumar.swaviman@outlook.com>
2024-01-29 21:46:25 +0100

    renaming prod tables

---
+++ SQL Scripts/Rename Production Tables.sql
@@ -0,0 +1,11 @@
+EXEC sp_rename 'Production.DimCurrency', 'Currency';
+
+EXEC sp_rename 'Production.DimCustomer', 'Customer';
+
+EXEC sp_rename 'Production.DimDate', 'Date';
+
+EXEC sp_rename 'Production.DimProduct', 'Product';
+
+EXEC sp_rename 'Production.FactInternetSales', 'Sales';
+
+
